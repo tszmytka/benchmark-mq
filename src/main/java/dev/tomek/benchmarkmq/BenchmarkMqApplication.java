1f40f47c571633450144bf9db5d697a125021bf6
Tomek Szmytka <tszmytka@gmail.com>
2020-10-17 13:18:44 +0200

    Prepare base structure

---
+++ src/main/java/dev/tomek/benchmarkmq/BenchmarkMqApplication.java
@@ -0,0 +1,13 @@
+package dev.tomek.benchmarkmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenchmarkMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenchmarkMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tomek/benchmarkmq/BenchmarkMqApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tomek.benchmarkmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenchmarkMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
