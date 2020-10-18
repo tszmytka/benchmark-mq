0a3139b9ce51d87ee34eea2be0b3f935a290ff44
Tomek Szmytka <tszmytka@gmail.com>
2020-10-18 22:50:47 +0200

    Run e2e with redis

--- manufacturer/src/main/java/dev/tomek/benchmarkmq/manufacturer/config/RedisConfig.java
+++ manufacturer/src/main/java/dev/tomek/benchmarkmq/manufacturer/config/RedisConfig.java
@@ -6,8 +6,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnection;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
