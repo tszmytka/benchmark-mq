373fd71762ab1629726dcb7f53a613f3e857d817
Tomek Szmytka <tszmytka@gmail.com>
2020-10-20 12:34:05 +0200

    Rabbitmq: use records

--- manufacturer/src/main/java/dev/tomek/benchmarkmq/manufacturer/comm/adapter/RabbitMqAdapter.java
+++ manufacturer/src/main/java/dev/tomek/benchmarkmq/manufacturer/comm/adapter/RabbitMqAdapter.java
@@ -1,16 +1,30 @@
 package dev.tomek.benchmarkmq.manufacturer.comm.adapter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.tomek.benchmarkmq.common.Airplane;
 import dev.tomek.benchmarkmq.common.Topic;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 
+import static dev.tomek.benchmarkmq.common.Profiles.COMM_RABBITMQ;
+
+@Log4j2
+@Component
+@Profile(COMM_RABBITMQ)
 @RequiredArgsConstructor
 public class RabbitMqAdapter implements CommAdapter {
     private final RabbitTemplate rabbitTemplate;
+    private final ObjectMapper objectMapper;
 
     @Override
     public void send(Airplane airplane, Topic topic) {
-        rabbitTemplate.convertAndSend(topic.toString(), "airplane hello world");
+        try {
+            rabbitTemplate.convertAndSend(topic.toString(), objectMapper.writeValueAsBytes(airplane));
+        } catch (Exception e) {
+            LOGGER.error("Cannot send message", e);
+        }
     }
 }
--- manufacturer/src/main/java/dev/tomek/benchmarkmq/manufacturer/config/RabbitMqConfig.java
+++ manufacturer/src/main/java/dev/tomek/benchmarkmq/manufacturer/config/RabbitMqConfig.java
@@ -7,7 +7,11 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+import static dev.tomek.benchmarkmq.common.Profiles.COMM_RABBITMQ;
+
+@Profile(COMM_RABBITMQ)
 @Configuration
 public class RabbitMqConfig {
     @Bean
