e86419a0c76c9b1b1ff3d7a39f985616cb5b7374
Tomek Szmytka <tszmytka@gmail.com>
2020-10-27 12:10:26 +0100

    Move config for nats into common class. Redis

--- manufacturer/src/main/java/dev/tomek/benchmarkmq/manufacturer/config/RedisConfig.java
+++ manufacturer/src/main/java/dev/tomek/benchmarkmq/manufacturer/config/RedisConfig.java
@@ -1,31 +1,19 @@
 package dev.tomek.benchmarkmq.manufacturer.config;
 
-import org.springframework.context.annotation.Bean;
+import dev.tomek.benchmarkmq.common.config.CommonRedisConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import static dev.tomek.benchmarkmq.common.Profiles.COMM_REDIS;
 
 @Configuration
 @Profile(COMM_REDIS)
-public class RedisConfig {
+public class RedisConfig extends CommonRedisConfig {
 
-    @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("dockerhost", 6379));
-    }
-
+    // you could also use a template
     public StringRedisTemplate redisTemplate(RedisConnectionFactory connectionFactory) {
         return new StringRedisTemplate(connectionFactory);
-    }
-
-    @Bean
-    public RedisConnection redisConnection(RedisConnectionFactory connectionFactory) {
-        return connectionFactory.getConnection();
     }
 }
--- patron/src/main/java/dev/tomek/benchmarkmq/patron/config/RedisConfig.java
+++ patron/src/main/java/dev/tomek/benchmarkmq/patron/config/RedisConfig.java
@@ -1,25 +1,13 @@
 package dev.tomek.benchmarkmq.patron.config;
 
-import org.springframework.context.annotation.Bean;
+import dev.tomek.benchmarkmq.common.config.CommonRedisConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.redis.connection.RedisConnection;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 import static dev.tomek.benchmarkmq.common.Profiles.COMM_REDIS;
 
 @Configuration
 @Profile(COMM_REDIS)
-public class RedisConfig {
-    @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("dockerhost", 6379));
-    }
+public class RedisConfig extends CommonRedisConfig {
 
-    @Bean
-    public RedisConnection redisConnection(RedisConnectionFactory connectionFactory) {
-        return connectionFactory.getConnection();
-    }
 }
