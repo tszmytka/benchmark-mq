f358ed243c1655fc82885ff4200d489c7a387060
Tomek Szmytka <tszmytka@gmail.com>
2020-11-07 19:20:04 +0100

    Set up testcontainers. Convert test for nats to use it.

--- patron/src/test/java/dev/tomek/benchmarkmq/patron/PatronNatsTest.java
+++ patron/src/test/java/dev/tomek/benchmarkmq/patron/PatronNatsTest.java
@@ -2,16 +2,39 @@ package dev.tomek.benchmarkmq.patron;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.lang.NonNull;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import static dev.tomek.benchmarkmq.common.Profiles.COMM_NATS;
-import static dev.tomek.benchmarkmq.common.Profiles.COMM_RABBITMQ;
 
 @SpringBootTest
 @ActiveProfiles(COMM_NATS)
+@Testcontainers
+@ContextConfiguration(initializers = {PatronNatsTest.Initializer.class})
 class PatronNatsTest {
+    private static final int NATS_PORT = 4222;
+
+    @Container
+    public static GenericContainer<?> nats = new GenericContainer<>(DockerImageName.parse("nats:2.1.8")).withExposedPorts(NATS_PORT);
 
     @Test
     void contextLoads() {
+    }
+
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(@NonNull ConfigurableApplicationContext applicationContext) {
+            final String natsHost = nats.getHost() + ":" + nats.getMappedPort(NATS_PORT);
+            TestPropertyValues.of("nats.spring.server=" + natsHost).applyTo(applicationContext);
+        }
     }
 }
